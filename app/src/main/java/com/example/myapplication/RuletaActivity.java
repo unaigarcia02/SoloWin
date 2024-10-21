ed23aa11ed70ef0f22aa200ef90fd88b4c44a023
unaigarcia02 <ugarcia060@ikasle.ehu.eus>
2024-10-21 19:17:37 +0200

    Agregadas todas las fichas, arreglado cuado sale 0 y hay un error en las fisicas que lo he solucionado para mi movil, no se arreglarlo global

--- app/src/main/java/com/example/myapplication/RuletaActivity.java
+++ app/src/main/java/com/example/myapplication/RuletaActivity.java
@@ -1,14 +1,11 @@
 package com.example.myapplication;
 
 import android.annotation.SuppressLint;
-import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.DisplayCutout;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewTreeObserver;
-import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.RotateAnimation;
 import android.widget.Button;
@@ -20,7 +17,9 @@ import android.widget.Toast;
 
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
 
 public class RuletaActivity extends BaseActivity {
@@ -29,11 +28,11 @@ public class RuletaActivity extends BaseActivity {
     private float currentDegree = 0f;
     private float dX, dY;
     private boolean isOriginalFicha;
-    private ImageView ficha1;
     private float saldo;
     private float[][] buttonCoordinates = new float[49][2];
-
     ImageButton[] botones = new ImageButton[49]; // Crear un array para almacenar los botones
+    private Map<ImageView, Integer> fichaCostMap = new HashMap<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,8 +42,19 @@ public class RuletaActivity extends BaseActivity {
 
         rouletteImage = findViewById(R.id.ruleta);
         Button spinButton = findViewById(R.id.spinButton);
-        ficha1 = findViewById(R.id.ficha10);
-        setupDraggableFicha(ficha1);
+
+        ImageView ficha10 = findViewById(R.id.ficha10);
+        ImageView ficha20 = findViewById(R.id.ficha20);
+        ImageView ficha50 = findViewById(R.id.ficha50);
+        ImageView ficha100 = findViewById(R.id.ficha100);
+        ImageView ficha500 = findViewById(R.id.ficha500);
+
+
+        setupDraggableFicha(ficha10, 10);
+        setupDraggableFicha(ficha20, 20);
+        setupDraggableFicha(ficha50, 50);
+        setupDraggableFicha(ficha100, 100);
+        setupDraggableFicha(ficha500, 500);
         inicializarBotones();
         spinButton.setOnClickListener(v -> spinRoulette());
 
@@ -58,6 +68,7 @@ public class RuletaActivity extends BaseActivity {
         // Generar un ángulo aleatorio para que la ruleta se detenga
         Random random = new Random();
         int randomDegree = random.nextInt(360) + 1080;  // +720 para que gire varias veces
+
 
         // Crear una animación de rotación
         RotateAnimation rotateAnimation = new RotateAnimation(
@@ -107,6 +118,7 @@ public class RuletaActivity extends BaseActivity {
         // Determinar en qué sección cae la ruleta
         int section = (int) (degreeNormalized / sectionAngle);
 
+
         // Mapeo de cada sección a un número de la ruleta (0-36)
         String[] resultMapping = {
                 "0", "32", "15", "19", "4", "21", "2", "25", "17", "34",
@@ -119,13 +131,17 @@ public class RuletaActivity extends BaseActivity {
         if (section < 0 || section >= resultMapping.length) {
             return "Resultado no válido";  // Manejo de error si la sección está fuera de rango
         }
+        if(section == 0){
+            section = 37;
+        }
 
         // Debug: Imprimir el ángulo y la sección
-        Log.d("Ruleta", "Grados Normalizados: " + currentDegree + ", Sección: " + section);
-        Log.d("resultado", resultMapping[resultMapping.length-section]);
+        Log.d("Ruleta", "Grados Normalizados: " + degreeNormalized + ", Sección: " + section);
+        Log.d("resultado", resultMapping[resultMapping.length - section]);
 
-        return resultMapping[resultMapping.length-section];
+        return resultMapping[resultMapping.length - section]; // Retorna el resultado basado en el índice ajustado
     }
+
 
     //SECCION PARA GESTIOAR LOS BOTONES DE LA RULETA
 
@@ -149,7 +165,7 @@ public class RuletaActivity extends BaseActivity {
                     int[] location = new int[2];
                     botones[i].getLocationOnScreen(location);
                     buttonCoordinates[i][0] = location[0]; // Coordenada X
-                    buttonCoordinates[i][1] = location[1]-30; // Coordenada Y
+                    buttonCoordinates[i][1] = location[1]; // Coordenada Y
                 }
                 // Quitar el listener para evitar llamar esto repetidamente
                 rootView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
@@ -158,72 +174,77 @@ public class RuletaActivity extends BaseActivity {
     }
 
     @SuppressLint("ClickableViewAccessibility")
-    private void setupDraggableFicha(ImageView ficha) {
+    private void setupDraggableFicha(ImageView ficha, int costo) {
+        // Inicializamos el "tag" de la ficha para saber si ya ha sido soltada
+        ficha.setTag(false);  // `false` indica que la ficha es nueva y no ha sido soltada
+
         ficha.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent) {
                 switch (motionEvent.getAction()) {
                     case MotionEvent.ACTION_DOWN:
-                        // Verificar si el saldo es suficiente antes de crear una nueva ficha
-                        if (saldo >= 10) {
-                            List<Integer> buttonIndices = getButtonsIndicesAtPosition(motionEvent.getRawX(), motionEvent.getRawY(), view.getWidth(), view.getHeight());
-
-                            // Si está sobre un botón, solo se arrastra la ficha existente
-                            if (!buttonIndices.isEmpty()) {
-                                dX = view.getX() - motionEvent.getRawX();
-                                dY = view.getY() - motionEvent.getRawY();
-                            } else {
-
-                                // Crear una nueva ficha
-                                ImageView newFicha = createNewFicha(view);
-                                setupDraggableFicha(newFicha);
-                                dX = newFicha.getX() - motionEvent.getRawX();
-                                dY = newFicha.getY() - motionEvent.getRawY();
-                            }
+                        // Solo inicializamos el arrastre de la ficha aquí, sin modificar el saldo
+                        List<Integer> buttonIndices = getButtonsIndicesAtPosition(motionEvent.getRawX(), motionEvent.getRawY(), view.getWidth(), view.getHeight());
+                        if (!buttonIndices.isEmpty()) {
+                            // Solo arrastrar la ficha existente
+                            dX = view.getX()+30 - motionEvent.getRawX();
+                            dY = view.getY()+30 - motionEvent.getRawY();
                         } else {
-                            // Mostrar un mensaje si no hay suficiente saldo
-                            ImageView newFicha = createNewFicha(view);
-                            setupDraggableFicha(newFicha);
-                            Toast.makeText(RuletaActivity.this, "Saldo insuficiente", Toast.LENGTH_SHORT).show();
+                            // Crear una nueva ficha sin descontar saldo
+                            ImageView newFicha = createNewFicha(view, costo);
+                            setupDraggableFicha(newFicha, costo); // Asociar el costo de la nueva ficha
+                            dX = newFicha.getX()+30 - motionEvent.getRawX();
+                            dY = newFicha.getY()+30 - motionEvent.getRawY();
                         }
                         break;
 
                     case MotionEvent.ACTION_MOVE:
+                        // Permitir que la ficha se arrastre sin costo
                         view.animate()
-                                .x(motionEvent.getRawX() + dX)
-                                .y(motionEvent.getRawY() + dY)
+                                .x(motionEvent.getRawX()+30 + dX)
+                                .y(motionEvent.getRawY()+30 + dY)
                                 .setDuration(0)
                                 .start();
                         break;
 
                     case MotionEvent.ACTION_UP:
+                        // Obtener las dimensiones de la ficha
                         int fichaWidth = view.getWidth();
                         int fichaHeight = view.getHeight();
+                        // Obtener los botones en los que se ha soltado la ficha
                         List<Integer> buttonIndicesOnRelease = getButtonsIndicesAtPosition(motionEvent.getRawX(), motionEvent.getRawY(), fichaWidth, fichaHeight);
 
                         if (!buttonIndicesOnRelease.isEmpty()) {
-                            // Verificar si el saldo es suficiente antes de permitir que la ficha se coloque
-                            if (saldo >= 10) {
-                                // Restar 10 del saldo al soltar la ficha sobre un botón válido
-                                saldo -= 10;
-                                actualizarSaldo();  // Actualiza el saldo en la interfaz de usuario
-
-                                StringBuilder message = new StringBuilder("Ficha colocada sobre los botones: ");
-                                for (Integer index : buttonIndicesOnRelease) {
-                                    message.append(index).append(" ");
-                                }
-                                Toast.makeText(RuletaActivity.this, message.toString(), Toast.LENGTH_SHORT).show();
+                            // Verificar si el saldo es suficiente antes de descontar
+                            if (saldo >= costo) {
+                                // Comprobar si esta es la primera vez que se suelta la ficha
+                                if (!(boolean) view.getTag()) {
+                                    // Si es la primera vez, descontamos el saldo
+                                    saldo -= costo;
+                                    actualizarSaldo();  // Actualiza el saldo en la interfaz de usuario
+                                    view.setTag(true);  // Marcamos la ficha como ya soltada
+                                    StringBuilder message = new StringBuilder("Ficha colocada sobre los botones: ");
+                                    for (Integer index : buttonIndicesOnRelease) {
+                                        message.append(index).append(" ");
+                                    }
+                                    Toast.makeText(RuletaActivity.this, message.toString(), Toast.LENGTH_SHORT).show();
+                                } else {
+                                    StringBuilder message2 = new StringBuilder("Ficha colocada sobre los botones: ");
+                                    for (Integer index : buttonIndicesOnRelease) {
+                                        message2.append(index).append(" ");
+                                    }
+                                    Toast.makeText(RuletaActivity.this, message2.toString(), Toast.LENGTH_SHORT).show();                                                                    }
                             } else {
-                                // Mostrar un mensaje si no hay suficiente saldo
+                                // Mostrar mensaje si no hay saldo suficiente
                                 Toast.makeText(RuletaActivity.this, "Saldo insuficiente", Toast.LENGTH_SHORT).show();
                                 view.setVisibility(View.GONE);  // Eliminar la ficha si no hay saldo
                             }
                         } else {
+                            // Si la ficha no se suelta sobre un botón válido, la eliminamos
                             view.setVisibility(View.GONE);
                             Toast.makeText(RuletaActivity.this, "Ficha no colocada sobre ningún botón. Ficha desaparece.", Toast.LENGTH_SHORT).show();
                         }
                         break;
-
 
                     default:
                         return false;
@@ -232,6 +253,7 @@ public class RuletaActivity extends BaseActivity {
             }
         });
     }
+
 
 
 
@@ -248,8 +270,8 @@ public class RuletaActivity extends BaseActivity {
                 // Obtener las coordenadas y tamaño del botón
                 int[] buttonLocation = new int[2];
                 button.getLocationOnScreen(buttonLocation);
-                int buttonX = buttonLocation[0]+27;
-                int buttonY = buttonLocation[1]+30;
+                int buttonX = buttonLocation[0];
+                int buttonY = buttonLocation[1];
                 int buttonWidth = button.getWidth();
                 int buttonHeight = button.getHeight();
 
@@ -277,18 +299,23 @@ public class RuletaActivity extends BaseActivity {
 
 
 
-    private ImageView createNewFicha(View originalFicha) {
+    private ImageView createNewFicha(View originalFicha, int costo) {
         RelativeLayout layout = findViewById(R.id.fichas_container);
 
+        // Crear una nueva ficha
         ImageView newFicha = new ImageView(RuletaActivity.this);
-        newFicha.setImageResource(R.drawable.ficha10);
 
+        // Obtener el recurso de imagen de la ficha original
+        newFicha.setImageDrawable(((ImageView) originalFicha).getDrawable());
+
+        // Mantener el tamaño original de la ficha
         int originalWidth = originalFicha.getWidth();
         int originalHeight = originalFicha.getHeight();
 
         RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(originalWidth, originalHeight);
         newFicha.setLayoutParams(params);
 
+        // Obtener la posición de la ficha original en la pantalla
         int[] locationOriginal = new int[2];
         originalFicha.getLocationOnScreen(locationOriginal);
 
@@ -298,17 +325,19 @@ public class RuletaActivity extends BaseActivity {
         int relativeX = locationOriginal[0] - locationLayout[0];
         int relativeY = locationOriginal[1] - locationLayout[1];
 
+        // Posicionar la nueva ficha en la misma posición que la ficha original
         newFicha.setX(relativeX);
         newFicha.setY(relativeY);
 
+        // Añadir la nueva ficha al contenedor
         layout.addView(newFicha);
 
         return newFicha;
     }
 
     private void actualizarSaldo() {
-        TextView saldoTextView = findViewById(R.id.Saldo); // Asegúrate de que tienes un TextView con id "Saldo"
-        saldoTextView.setText(String.valueOf(saldo));  // Actualiza el texto del saldo
+        TextView saldoTextView = findViewById(R.id.Saldo);
+        saldoTextView.setText(String.valueOf(saldo));
     }
 }
 
