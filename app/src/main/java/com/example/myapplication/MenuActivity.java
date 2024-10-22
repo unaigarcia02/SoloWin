8aaef7dd5448dbfebc2cc8ddcdca3b045add5bbc
unaigarcia02 <ugarcia060@ikasle.ehu.eus>
2024-10-22 21:24:30 +0200

    arreglado una pijada mia

--- app/src/main/java/com/example/myapplication/MenuActivity.java
+++ app/src/main/java/com/example/myapplication/MenuActivity.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import android.content.Intent;
 import android.os.Bundle;
 
+import android.view.View;
 import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
@@ -18,7 +19,6 @@ public class MenuActivity extends BaseActivity  {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_menu);
-
 
         Button ruletaBtn = findViewById(R.id.RuletaBTN);
         Button buscaminasBtn = findViewById(R.id.BuscaminasBTN);
