e90a584891ab1ede9cc3ee070e0d6795b109ca58
ibai <ibaisologestoa71@gmail.com>
2024-11-11 00:55:00 +0100

    o7

--- app/src/main/java/com/example/myapplication/BlackJackActivity.java
+++ app/src/main/java/com/example/myapplication/BlackJackActivity.java
@@ -13,6 +13,10 @@ import java.util.Random;
 public class BlackJackActivity extends AppCompatActivity {
     private float saldo = BaseActivity.saldo; //utiliza esta variable como saldo, porque se actualiza para todos los juegos
     private ActivityBlackJackBinding binding;
+    private int numeroactual = 0;
+
+    private int dealerpunt= 0;
+    private int jugadorpunt=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,15 +54,45 @@ public class BlackJackActivity extends AppCompatActivity {
         //Esto de aqui abajo usa el nombre y lo convierte en ID para cambiar la imagen, inteligente si me preguntas
         binding.imageView11.setImageResource(getResources().getIdentifier(carta, "drawable", getPackageName()));
         binding.imageView11.setVisibility(View.VISIBLE);
+        dealerpunt = actualizarPuntuacion(numeroactual,dealerpunt);
+        binding.dealerScore.setText(String.valueOf("Puntuación: "+dealerpunt));
+
+        String carta2 = sacarCarta();
+        binding.imageView17.setImageResource(getResources().getIdentifier(carta2, "drawable", getPackageName()));
+        binding.imageView17.setVisibility(View.VISIBLE);
+        jugadorpunt = actualizarPuntuacion(numeroactual,jugadorpunt);
+        binding.playerScore.setText("Puntuación: "+String.valueOf(jugadorpunt));
 
 
 
 
 
     }
+    public int actualizarPuntuacion(int numero, int puntuacion) {
+        int puntos = 0;
+
+        switch (numero) {
+            case 1:
+                puntos = 11;
+                break;
+            case 11:
+            case 12:
+            case 13:
+                puntos = 10;
+                break;
+            default:
+                puntos = numero;
+                break;
+        }
+
+        puntuacion += puntos;
+
+        Toast.makeText(this, "Puntuación actual: " + puntuacion, Toast.LENGTH_SHORT).show();
+        return puntos;
+    }
     public String sacarCarta(){
         Random random = new Random();
-        int palo = random.nextInt(4);
+        int palo = random.nextInt(3)+1;
         String palon = "";
         switch (palo){
             case 0:
@@ -74,9 +108,9 @@ public class BlackJackActivity extends AppCompatActivity {
                 palon = "clubs";
                 break;
         }
-        int numero = random.nextInt(14);
+        numeroactual = random.nextInt(13)+1;
         String carta = "";
-        switch (numero) {
+        switch (numeroactual) {
             case 1:
                 carta = "ace";
                 break;
@@ -90,7 +124,7 @@ public class BlackJackActivity extends AppCompatActivity {
                 carta = "king";
                 break;
             default:
-                carta = String.format("%02d", numero);
+                carta = String.format("%02d", numeroactual);
                 break;
         }
 
