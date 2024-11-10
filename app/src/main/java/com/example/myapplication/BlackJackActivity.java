5e6d8916f53f7c759c5891782ccb853cb7ddce53
ibai <ibaisologestoa71@gmail.com>
2024-11-10 23:29:21 +0100

    añadida minima funcionalidad a botones y saldo se actualiza correctamente

--- app/src/main/java/com/example/myapplication/BlackJackActivity.java
+++ app/src/main/java/com/example/myapplication/BlackJackActivity.java
@@ -1,13 +1,14 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.myapplication.databinding.ActivityBlackJackBinding;
 
 public class BlackJackActivity extends AppCompatActivity {
-
+    private float saldo = BaseActivity.saldo; //utiliza esta variable como saldo, porque se actualiza para todos los juegos
     private ActivityBlackJackBinding binding;
 
     @Override
@@ -16,5 +17,33 @@ public class BlackJackActivity extends AppCompatActivity {
 
         binding = ActivityBlackJackBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        binding.saldoText.setText(String.valueOf(saldo));
+        setupButtons();
     }
+
+    private void setupButtons() {
+        binding.pedirButton.setOnClickListener(v -> {
+            Toast.makeText(this, "QUE", Toast.LENGTH_SHORT).show();
+        });
+
+        binding.plantarseButton.setOnClickListener(v -> {
+            Toast.makeText(this, "QUE", Toast.LENGTH_SHORT).show();
+        });
+
+        binding.doblarButton.setOnClickListener(v -> {
+            Toast.makeText(this, "QUE", Toast.LENGTH_SHORT).show();
+        });
+
+        binding.repartirButton.setOnClickListener(v -> {
+            repartir();
+        });
+    }
+    public void repartir(){
+        binding.pedirButton.setEnabled(true);
+        binding.plantarseButton.setEnabled(true);
+        binding.doblarButton.setEnabled(true);
+
+    }
+
 }
+
