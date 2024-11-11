24d9942d18a921e1efd2120f4630e83f3e320f6f
ibai <ibaisologestoa71@gmail.com>
2024-11-12 00:41:50 +0100

    Se ha arreglado el menu de inicio, se ha optimizado para que pare el video y vuelva cuando sea necesario y se ha corregido que la imagen y el boton no estuviesen alineados

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,8 +1,6 @@
 package com.example.myapplication;
 
 import android.content.Intent;
-import android.graphics.RenderEffect;
-import android.graphics.Shader;
 import android.net.Uri;
 import android.os.Bundle;
 
@@ -13,20 +11,18 @@ import androidx.core.view.WindowInsetsCompat;
 
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.VideoView;
 
-
-public class MainActivity extends BaseActivity  {
+public class MainActivity extends BaseActivity {
     private VideoView video;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-
-        video=(VideoView) findViewById(R.id.videoView);
+        video = findViewById(R.id.videoView);
         String path = "android.resource://" + getPackageName() + "/" + R.raw.casin;
         video.setVideoURI(Uri.parse(path));
         video.setOnCompletionListener(mediaPlayer -> video.start());
@@ -34,14 +30,10 @@ public class MainActivity extends BaseActivity  {
         video.start();
 
         Button buttonJugar = findViewById(R.id.buttonJgr);
-
-        // Asigna un listener para navegar a la segunda actividad
         buttonJugar.setOnClickListener(v -> {
             Intent intent = new Intent(MainActivity.this, MenuActivity.class);
             startActivity(intent);
         });
-
-
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
@@ -50,5 +42,19 @@ public class MainActivity extends BaseActivity  {
         });
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (video != null) {
+            video.start(); // Reanudar el video cuando la actividad se vuelve visible
+        }
+    }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (video != null) {
+            video.pause(); // Pausar el video cuando la actividad está en segundo plano
+        }
+    }
 }
