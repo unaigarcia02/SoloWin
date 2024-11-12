49e9e1bd1ea002222bbca00bf665f97f2ccce92f
ibai <ibaisologestoa71@gmail.com>
2024-11-13 00:13:32 +0100

    Se ha hecho ia dealer, hecho el botón de plantarse, mejorado el codigo y añadidas mas cartas posibles para que no se quede pequeño

--- app/src/main/java/com/example/myapplication/BlackJackActivity.java
+++ app/src/main/java/com/example/myapplication/BlackJackActivity.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.ImageView;
@@ -21,14 +22,15 @@ public class BlackJackActivity extends AppCompatActivity {
     private ActivityBlackJackBinding binding;
     private int numeroactual = 0;
 
-    private int dealerpunt= 0;
-    private int jugadorpunt=0;
+    private int dealerpunt = 0;
+    private int jugadorpunt = 0;
     private double apuesta = 0;
 
     private ImageView[] pcs;
     private ImageView[] dcs;
 
     private List<String> usadas = new ArrayList<>();
+
     //al cambiar de foco vuelve a tener pantalla completa
     @Override
     public void onWindowFocusChanged(boolean hasFocus) {
@@ -45,6 +47,7 @@ public class BlackJackActivity extends AppCompatActivity {
             );
         }
     }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -66,7 +69,7 @@ public class BlackJackActivity extends AppCompatActivity {
                         View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
                         View.SYSTEM_UI_FLAG_FULLSCREEN
         );
-          pcs = new ImageView[] {
+        pcs = new ImageView[]{
                 binding.pc1,
                 binding.pc2,
                 binding.pc3,
@@ -74,7 +77,7 @@ public class BlackJackActivity extends AppCompatActivity {
                 binding.pc5,
                 binding.pc6
         };
-        dcs = new ImageView[]  {
+        dcs = new ImageView[]{
                 binding.dc1,
                 binding.dc2,
                 binding.dc3,
@@ -90,7 +93,7 @@ public class BlackJackActivity extends AppCompatActivity {
         });
 
         binding.plantarseButton.setOnClickListener(v -> {
-            //plantarse();
+            plantarse();
         });
 
         binding.doblarButton.setOnClickListener(v -> {
@@ -103,7 +106,7 @@ public class BlackJackActivity extends AppCompatActivity {
                 if (apuesta <= saldo) {
                     saldo = (float) (saldo - apuesta);
                     binding.saldoText.setText(String.valueOf(saldo));
-                    binding.apostado.setText("Apostado: "+apuesta);
+                    binding.apostado.setText("Apostado: " + apuesta);
                     repartir();
                 } else {
                     Toast.makeText(this, "No tienes suficiente saldo", Toast.LENGTH_SHORT).show();
@@ -117,6 +120,7 @@ public class BlackJackActivity extends AppCompatActivity {
         });
 
     }
+
     private void mostrarInstrucciones() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("Instrucciones de Blackjack");
@@ -129,7 +133,26 @@ public class BlackJackActivity extends AppCompatActivity {
         });
         builder.show();
     }
-    private void repartir(){
+
+    private void repartir() {
+        jugadorpunt = 0;
+        dealerpunt = 0;
+        binding.playerScore.setText("Puntuación: " + String.valueOf(jugadorpunt));
+        binding.dealerScore.setText("Puntuación: " + String.valueOf(dealerpunt));
+
+
+        for (ImageView pc : pcs) {
+            pc.setVisibility(View.INVISIBLE);
+        }
+        for (ImageView dc : dcs) {
+            dc.setVisibility(View.INVISIBLE);
+        }
+        binding.pedirButton.setEnabled(false);
+        binding.plantarseButton.setEnabled(false);
+        binding.doblarButton.setEnabled(false);
+        binding.apostado.setText("Apostado: " + 0.0);
+
+        // aqui separo yo
         binding.pedirButton.setEnabled(true);
         binding.plantarseButton.setEnabled(true);
         binding.doblarButton.setEnabled(true);
@@ -138,22 +161,27 @@ public class BlackJackActivity extends AppCompatActivity {
         //Esto de aqui abajo usa el nombre y lo convierte en ID para cambiar la imagen, inteligente si me preguntas
         binding.dc1.setImageResource(getResources().getIdentifier(carta, "drawable", getPackageName()));
         binding.dc1.setVisibility(View.VISIBLE);
-        dealerpunt = actualizarPuntuacion(numeroactual,dealerpunt);
-        binding.dealerScore.setText(String.valueOf("Puntuación conocida: "+dealerpunt));
+        dealerpunt = actualizarPuntuacion(numeroactual, dealerpunt);
+        binding.dealerScore.setText(String.valueOf("Puntuación conocida: " + dealerpunt));
         binding.dc2.setVisibility(View.VISIBLE);
+        int randomback = new Random().nextInt(8) + 1;
+        String randombackS = String.valueOf(randomback);
+        binding.dc2.setImageResource(getResources().getIdentifier("back0" + randombackS, "drawable", getPackageName()));
+
 
         String carta2 = sacarCarta();
         binding.pc1.setImageResource(getResources().getIdentifier(carta2, "drawable", getPackageName()));
         binding.pc1.setVisibility(View.VISIBLE);
-        jugadorpunt = actualizarPuntuacion(numeroactual,jugadorpunt);
+        jugadorpunt = actualizarPuntuacion(numeroactual, jugadorpunt);
         String carta3 = sacarCarta();
         binding.pc2.setImageResource(getResources().getIdentifier(carta3, "drawable", getPackageName()));
         binding.pc2.setVisibility(View.VISIBLE);
-        jugadorpunt = actualizarPuntuacion(numeroactual,jugadorpunt);
-        binding.playerScore.setText("Puntuación: "+String.valueOf(jugadorpunt));
+        jugadorpunt = actualizarPuntuacion(numeroactual, jugadorpunt);
+        binding.playerScore.setText("Puntuación: " + String.valueOf(jugadorpunt));
 
 
     }
+
     private int actualizarPuntuacion(int numero, int puntuacion) {
         int puntos = 0;
 
@@ -172,13 +200,12 @@ public class BlackJackActivity extends AppCompatActivity {
         }
 
         puntuacion += puntos;
-        if(jugadorpunt > 21 || dealerpunt > 21)
-        {
+        if (jugadorpunt > 21 || dealerpunt > 21) {
             Toast.makeText(this, "Has perdido", Toast.LENGTH_SHORT).show();
             jugadorpunt = 0;
-            dealerpunt =0;
-            binding.playerScore.setText("Puntuación: "+String.valueOf(jugadorpunt));
-            binding.dealerScore.setText("Puntuación: "+String.valueOf(dealerpunt));
+            dealerpunt = 0;
+            binding.playerScore.setText("Puntuación: " + String.valueOf(jugadorpunt));
+            binding.dealerScore.setText("Puntuación: " + String.valueOf(dealerpunt));
 
 
             for (ImageView pc : pcs) {
@@ -190,20 +217,21 @@ public class BlackJackActivity extends AppCompatActivity {
             binding.pedirButton.setEnabled(false);
             binding.plantarseButton.setEnabled(false);
             binding.doblarButton.setEnabled(false);
+            binding.apostado.setText("Apostado: " + 0.0);
 
             //Toast.makeText(this, String.valueOf(jugadorpunt), Toast.LENGTH_SHORT).show();
             return 0;
 
         }
-
+        // if puntuacion == 21 black jack
         return puntuacion;
     }
 
-    private String sacarCarta(){
+    private String sacarCarta() {
         Random random = new Random();
         int palo = random.nextInt(4);
         String palon = "";
-        switch (palo){
+        switch (palo) {
             case 0:
                 palon = "spades";
                 break;
@@ -217,7 +245,7 @@ public class BlackJackActivity extends AppCompatActivity {
                 palon = "clubs";
                 break;
         }
-        numeroactual = random.nextInt(13)+1;
+        numeroactual = random.nextInt(13) + 1;
         String carta = "";
         switch (numeroactual) {
             case 1:
@@ -237,27 +265,85 @@ public class BlackJackActivity extends AppCompatActivity {
                 break;
         }
 
-        if(usadas.contains(palon+"_"+carta)){
-            Toast.makeText(this,"Se ha eliminado"+palon+"_"+carta, Toast.LENGTH_SHORT).show();
+        if (usadas.contains(palon + "_" + carta)) {
+            Toast.makeText(this, "Se ha eliminado" + palon + "_" + carta, Toast.LENGTH_SHORT).show();
             return sacarCarta();
         }
-        usadas.add(palon+"_"+carta);
-        return palon+"_"+carta;
+        usadas.add(palon + "_" + carta);
+        return palon + "_" + carta;
     }
 
-    private void pedir(){
+    private void pedir() {
         for (ImageView pc : pcs) {
-            if(pc.getVisibility() != View.VISIBLE) {
+            if (pc.getVisibility() != View.VISIBLE) {
                 String carta = sacarCarta();
                 pc.setImageResource(getResources().getIdentifier(carta, "drawable", getPackageName()));
                 pc.setVisibility(View.VISIBLE);
-                jugadorpunt = actualizarPuntuacion(numeroactual,jugadorpunt);
-                binding.playerScore.setText("Puntuación: "+String.valueOf(jugadorpunt));
+                jugadorpunt = actualizarPuntuacion(numeroactual, jugadorpunt);
+                binding.playerScore.setText("Puntuación: " + String.valueOf(jugadorpunt));
                 break;
             }
         }
     }
+
+    private void plantarse() {
+        binding.pedirButton.setEnabled(false);
+        binding.plantarseButton.setEnabled(false);
+        binding.doblarButton.setEnabled(false);
+
+        // Mostrar la primera carta del dealer
+        String carta = sacarCarta();
+        binding.dc2.setImageResource(getResources().getIdentifier(carta, "drawable", getPackageName()));
+        dealerpunt = actualizarPuntuacion(numeroactual, dealerpunt);
+        binding.dealerScore.setText("Puntuación: " + dealerpunt);
+
+        // El Handler para manejar el retraso entre cartas
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (dealerpunt < 17) {
+                    for (ImageView dc : dcs) {
+                        if (dc.getVisibility() != View.VISIBLE) {
+                            String carta = sacarCarta();
+                            dc.setImageResource(getResources().getIdentifier(carta, "drawable", getPackageName()));
+                            dc.setVisibility(View.VISIBLE);
+                            dealerpunt = actualizarPuntuacion(numeroactual, dealerpunt);
+                            binding.dealerScore.setText("Puntuación: " + dealerpunt);
+                            break;
+                        }
+                    }
+                    handler.postDelayed(this, 500);
+                } else if (dealerpunt > 21) {
+                    Toast.makeText(binding.getRoot().getContext(), "El dealer se pasó, ¡ganas!", Toast.LENGTH_SHORT).show();
+                    saldo = (float) (saldo + 2*apuesta);
+                    binding.saldoText.setText(String.valueOf(saldo));
+
+                } else {
+                    Toast.makeText(binding.getRoot().getContext(), "El dealer se planta con " + dealerpunt, Toast.LENGTH_SHORT).show();
+                    if(dealerpunt > jugadorpunt) {
+                        Toast.makeText(binding.getRoot().getContext(), "Has perdido, el dealer tiene más puntuación", Toast.LENGTH_SHORT).show();
+                    } //apuesta a 0?
+                    else if (dealerpunt < jugadorpunt) {
+                        Toast.makeText(binding.getRoot().getContext(), "Has ganado, tu tienes más puntuación", Toast.LENGTH_SHORT).show();
+                        saldo = (float) (saldo + 2*apuesta);
+                        binding.saldoText.setText(String.valueOf(saldo));
+
+                    }
+                    else if(dealerpunt == jugadorpunt){
+                        Toast.makeText(binding.getRoot().getContext(), "¿Empate? El casino ha devuelto su apuesta", Toast.LENGTH_SHORT).show();
+                        saldo = (float) (saldo + apuesta);
+                        binding.saldoText.setText(String.valueOf(saldo));
+
+
+                    }
+
+                    }
+            }
+        }, 500); // 500 ms de retraso entre cada carta
     }
+}
+
 
 
 
